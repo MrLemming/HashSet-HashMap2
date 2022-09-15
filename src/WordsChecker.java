3226c2f02e2eecdd63818491bf784a7a42123124
Aleksey Esin <aesin87@mail.ru>
2022-09-15 20:56:11 +0300

    Initial commit

---
+++ src/WordsChecker.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordsChecker {
+    protected String text;
+
+    public WordsChecker(String text) {
+
+        this.text = text;
+    }
+
+    public boolean hasWord(String word) {
+        Set<String> set = new HashSet<>(List.of(text.split("\\P{IsAlphabetic}+")));
+        return set.contains(word);
+    }
+}
